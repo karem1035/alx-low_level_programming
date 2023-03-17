033adf651aedf3109a74574647f9a90f77261424
Karem Darwesh <karemdarwesh1035@gmail.com>
2023-03-16 19:48:44 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prints with put function
+ *
+ * Return: Always 0 (Success)
+*/
+
 int main(void)
 {
-	char str[] = "\"Programming is like building a multilingual puzzle";
-	puts(str);
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
