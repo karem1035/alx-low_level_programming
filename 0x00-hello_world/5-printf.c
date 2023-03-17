de54c5f2d745020dcffa9d3b722886c34f71751d
Karem Darwesh <karemdarwesh1035@gmail.com>
2023-03-16 19:57:39 -0700

    5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: To print a sentence using printf function.
+ *
+ * Return: Always 0.
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art\,\n");
+
+	return (0);
+}
