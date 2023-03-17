b4ae83ce796a4624339066699a5593e4f9dc1401
Karem Darwesh <karemdarwesh1035@gmail.com>
2023-03-17 16:41:45 -0700

    9. Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - prints the decimal numbers.
+ * @void: take no parameters.
+ *
+ * Description: prints the decimal values from 0 to 9,
+ * with a , and a white space before them.
+ * Return: 0 always.
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		if (c != '9')
+		{
+			putchar (c);
+			putchar (',');
+			putchar (' ');
+		}
+		else
+		{
+			putchar(c);
+		}
+
+	}
+
+	return (0);
+}
