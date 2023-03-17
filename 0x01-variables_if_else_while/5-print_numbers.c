2b34ce6474661fc39a7668d367ff5580740fcdee
Karem Darwesh <karemdarwesh1035@gmail.com>
2023-03-17 15:08:18 -0700

    5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints number 1:9.
+ * @void: no parameters.
+ *
+ * Description: for loop prints 0 to 9.
+ * Return: zero always.
+ */
+
+int main(void)
+{
+	int i;
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+
+	printf("\n");
+	return (0);
+}
