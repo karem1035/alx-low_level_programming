1fd0b88f82ac023fa9377946e406892b7cedb254
Karem Darwesh <karemdarwesh1035@gmail.com>
2023-03-21 13:03:09 -0700

    3. islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isalpha - start point
+ * @c: takes an int as parameter.
+ *
+ * Description: checks if the letter is lowercase of now.
+ * Return: 1 if it is lowercase, 0 if otherwise.
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 'a; && c <= 'z')
+	{
+		return (1);
+	}
+	return (0);
+}
