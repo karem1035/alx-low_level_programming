f7b57d304d27c2bc97bd5a7e8b83f3c4c9c69c1a
Karem Darwesh <karemdarwesh1035@gmail.com>
2023-03-21 03:22:14 -0700

    tried something

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,16 @@
+#include <stdio.h>
 #include "_putchar.c"
-
 
 int main(void)
 {
-	_putchar('k');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 	return (0);
 }
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,3 @@
+#include "_putchar.c"
+
+int _putchar(char c);
