5618e5002b76bbbe4fa57b3f59b4b90b28e2dfe7
Karem Darwesh <karemdarwesh1035@gmail.com>
2023-03-21 19:47:30 -0700

    9. Learn your times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table
+ * @void: takes no parameter.
+ *
+ * Description: prints from 0 to 81 times 9.
+ * Return: 0.
+ */
+
+
+void times_table(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		int number = i * 9'
+
+		if (number == 81)
+		{
+			printf("%d", number);
+		}
+		else if (number < 9)
+		{
+			printf("%d,  ", number)
+		}
+		else
+		{
+			printf("%d, ");
+		}
+	}
+	return (0);
+}
