a878da71a06556aec0e2f39ff03b4c4f700f112d
Karem Darwesh <karemdarwesh1035@gmail.com>
2023-03-21 19:34:29 -0700

    10 add two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.c"
+
+/**
+ * add - adds two integers.
+ * @x: the first.
+ * @y: the second.
+ *
+ * Description: adds two integers parameters.
+ * Return: the sum of the two integers.
+ */
+
+int add(int x, int y)
+{
+	return (x + y);
+}
+
