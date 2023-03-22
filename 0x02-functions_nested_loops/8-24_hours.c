0829fd37e5a87f63bb01cb5e69ebda99a1295c6e
Karem Darwesh <karemdarwesh1035@gmail.com>
2023-03-21 23:10:09 -0700

    8. I'm federal agent Jack Bauer, and today is the longest day of my life

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,8 +1,41 @@
-#include "main"
+#include <stdio.h>
+
+/**
+ * jack_bauer - prints the time in scope
+ * @void: takes no parameter.
+ *
+ * Description: prints the time from 00:00 to 24:00
+ * Return: 0.
+ */
 
 void jack_bauer(void)
 {
-	int hours2, hours1, mins;
+	int hour;
 
-	while ()
+	for (hour = 0 ; hour < 24 ; hour++)
+	{
+
+		int min;
+
+		for (min = 0 ; min < 60 ; min++)
+		{
+			if (hour < 10)
+			{
+				printf("0%d:", hour);
+			}
+			else
+			{
+				printf("%d:", hour);
+			}
+
+			if (min < 10)
+			{
+				printf("0%d\n", min);
+			}
+			else
+			{
+				printf("%d\n", min);
+			}
+		}
+	}
 }
