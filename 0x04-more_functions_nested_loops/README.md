a071258e610bc0d5d0bda5b22f0b00eb08f1b656
Karem Darwesh <karemdarwesh1035@gmail.com>
2023-03-23 13:45:18 -0700

    checks if letter is upper or not

---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+# Here is my answers for task "0x04. C - More functions, more nested loops"
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,7 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+#include <stdio.h>
+
+int _isupper(int c);
+
+#endif
