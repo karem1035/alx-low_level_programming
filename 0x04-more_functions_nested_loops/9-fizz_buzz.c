a3cdb3023738647086cb7e3126a20622349764bc
Karem Darwesh <karemdarwesh1035@gmail.com>
2023-03-23 18:00:01 -0700

    print from 1 to 100 Fizz Buzz style

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,13 +15,17 @@ int main(void)
 
 	for (i = 1 ; i < 101 ; i++)
 	{
-		if (i % 3 == 0)
+		if ((i % 3 == 0) && (i % 4 != 0))
 		{
 			printf("Fizz ");
 		}
-		else if (i % 4 == 0)
+		else if ((i % 4 == 0) && (i % 3 != 0))
 		{
 			printf("Buzz ");
+		}
+		else if ((i % 3 == 0) && (i % 4 == 0))
+		{
+			printf("FizzBuzz ");
 		}
 		else
 		{
