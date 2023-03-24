1227d10b650903e63f63b3e7b7878ac00702ee9b
Karem Darwesh <karemdarwesh1035@gmail.com>
2023-03-23 17:32:15 -0700

    print a square with size

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -12,15 +12,22 @@ void print_square(int size)
 {
 	int i;
 
-	for (i = 0 ; i < size ; i++)
+	if (size <= 0)
 	{
-		int j;
-
-		for (j = 0 ; j < size ; j++)
-		{
-			_putchar('#');
-		}
-
 		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0 ; i < size ; i++)
+		{
+			int j;
+
+			for (j = 0 ; j < size ; j++)
+			{
+				_putchar('#');
+			}
+
+			_putchar('\n');
+		}
 	}
 }
