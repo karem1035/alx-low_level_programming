bc51561a7a9ca1d904f41914000b54968cd81533
Karem Darwesh <karemdarwesh1035@gmail.com>
2023-03-23 17:00:48 -0700

    draw a line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -3,24 +3,25 @@
 /**
  * print_line - prints _
  * @n: the length of the line
- * 
+ *
  * Description: prints a line of _
  * Return: 0.
 */
 
 void print_line(int n)
 {
-        if (n <= 0)
-        {
-                _putchar('\n');
-        }
-        else
-        {
-                int i;
-                for (i = 0 ; i < n ; i++)
-                {
-                        _putchar('_');
-                }
-                _putchar('\n');
-        }
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i;
+
+	for (i = 0 ; i < n ; i++)
+	{
+		_putchar('_');
+	}
+		_putchar('\n');
+	}
 }
