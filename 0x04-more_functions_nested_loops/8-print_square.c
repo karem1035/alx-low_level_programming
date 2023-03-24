f565ec4e4d968a67b00960011d474fe647078b42
Karem Darwesh <karemdarwesh1035@gmail.com>
2023-03-23 17:30:33 -0700

    print a square with size

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_square - print a square with size.
+ * @size: the size of the square.
+ *
+ * Description: printing a # times size square
+ * Return: void.
+ */
+
+void print_square(int size)
+{
+	int i;
+
+	for (i = 0 ; i < size ; i++)
+	{
+		int j;
+
+		for (j = 0 ; j < size ; j++)
+		{
+			_putchar('#');
+		}
+
+		_putchar('\n');
+	}
+}
