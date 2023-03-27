b382433c25a1d5afed9cce41d212870816233b2a
Karem Darwesh <karemdarwesh1035@gmail.com>
2023-03-27 15:41:30 -0700

    change the value of the pointer to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - changes the value of pointer.
+ * @*n: ponter as parameter
+ *
+ * Description: change the value where the pointer points to.
+ * Retrun: Void.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
