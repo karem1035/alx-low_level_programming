1b1d4280cc71abf73e015904404cb0320d93298b
Karem Darwesh <karemdarwesh1035@gmail.com>
2023-03-27 16:22:24 -0700

    add \n to the end of the string

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,6 +2,7 @@
 #define HEADER_FILE
 #include <stdio.h>
 
+int _putchar(char c);
 
 void reset_to_98(int *n);
 
@@ -9,5 +10,6 @@ void swap_int(int *a, int *b);
 
 int _strlen(char *s);
 
-#endif
+void _puts(char *str);
 
+#endif
