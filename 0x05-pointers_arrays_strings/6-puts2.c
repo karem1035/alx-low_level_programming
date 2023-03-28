b4fb4e5bd3cc551d1b8cebaa4f84ac0f19ce18f2
Karem Darwesh <karemdarwesh1035@gmail.com>
2023-03-27 18:36:29 -0700

    prints every second value of the string

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -13,19 +13,19 @@ void puts2(char *str)
 {
 	int i = 0, j;
 
-	while(str[i])
+	while (str[i])
 	{
 		i++;
 	}
 
 
-	for(j = 0 ; j < i ; j++)
+	for (j = 0 ; j < i ; j++)
 	{
-		if(j % 2 == 0 || i == 0)
+		if (j % 2 == 0 || i == 0)
 		{
 			_putchar(str[j]);
 		}
 	}
 
-	_putchar('\n';
+	_putchar('\n');
 }
