1e92e820154e2e7a85789ff3a455fa9ac17f9ee5
Karem Darwesh <karemdarwesh1035@gmail.com>
2023-03-30 08:18:36 -0700

    trial

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * *_strcat - cincatenates two strigs.
+ * @*dest: first string.
+ * @*src: second string.
+ *
+ * Return: *char/string.
+ */
+
+
+
+char *_strcat(char *dest, char *src)
+{
+	char *str3[sizeof(*dest) + sizeof()]
+	
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+# README File for 1. strncat
