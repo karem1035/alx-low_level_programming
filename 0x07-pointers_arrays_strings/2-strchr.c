b91fc1f16e5dfd80cf25a2ef00c008c384eb2f83
Karem Darwesh <karemdarwesh1035@gmail.com>
2023-04-03 06:24:38 -0700

    locate a location of a char in a string and returns it, or null if not found

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strchr - returns the address when certain char is located.
+ * @s: the string to be checked.
+ * @c: the letter we are searching for.
+ *
+ * Return: (NULL) if the letter hasn't located.
+ * Return: The address where the letter is located.
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		if (*(s + i) == c)
+		{
+			return (s + i);
+		}
+
+		i++;
+	}
+
+	return (NULL);
+}
