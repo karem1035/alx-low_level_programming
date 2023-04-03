eec545db4c2c156062bc5291ff537d21159bfd3b
Karem Darwesh <karemdarwesh1035@gmail.com>
2023-04-02 17:24:11 -0700

    convert string to integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -5,7 +5,6 @@
  * @s: the input string
  *
  * Return: total the resulting string.
- */
 int _atoi(char *s)
 {
 	int j, num, sign = 1;
@@ -38,3 +37,42 @@ int _atoi(char *s)
 	total *= sign;
 	return (total);
 }
+*/
+
+int _atoi(char *s)
+{
+        int j , num, sign = 1;
+        int total = 0;
+
+        for (j = 0 ; *(s + j) != '\0' ; j++)
+        {
+                if (*(s + j) == '-' || *(s + j) == '+')
+                {
+                        if (*(s + j) == '-')
+                        {
+                                sign = sign * -1;
+                        }
+                }
+             
+                else if (*(s + j) >= '0' && *(s + j) <= '9')
+                {
+                        break;
+                }
+        }
+
+        while (*(s + j) >= '0' && *(s + j) <= '9')
+        {
+                if (total > 0)
+                {
+                        total *= 10;
+                }
+                
+                num = *(s + j) - '0';
+                total = total + num;
+                j++;     
+        }
+        total *= sign;
+
+        return (total);
+}
+
