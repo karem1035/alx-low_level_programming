f9197bfaef3a254b19f61d16c4626f773d8db424
Karem Darwesh <karemdarwesh1035@gmail.com>
2023-04-03 22:31:31 -0700

    searches a string for any of a set of bytes

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,30 @@
+#include "man.h"
+
+/**
+ * _strpbrk - return the first occurence of a letter.
+ * @s: the string to be searched.
+ * @accept: the string contains letters.
+ *
+ * Return: s or Null where is the address of the first occurence of the letter.
+ */
+
+
+char *_strpbrk(char *s, char *accept)
+{
+	char *p;
+
+	while (*s)
+	{
+		for (p = accept ; *p ; p++)
+		{
+			if (*p == *s)
+			{
+				return (s);
+			}
+		}
+
+		s++;
+	}
+
+	return (NULL);
+}
