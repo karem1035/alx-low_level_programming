8014ecaaa4a29f8d3466e479e06b4ed7b705d715
Karem Darwesh <karemdarwesh1035@gmail.com>
2023-04-05 05:48:38 -0700

    calculates the factorial of a number

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -14,13 +14,10 @@ int factorial(int n)
 	{
 		return (1);
 	}
-	else if (n < 0)
-	{
-		return (-1);
-	}
-
-	if (n > 0)
+	else if (n > 0)
 	{
 		return (n * factorial(n - 1));
 	}
+
+	return (-1);
 }
