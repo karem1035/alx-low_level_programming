ecbf71e07945ee4ab715dc0f9e77e6fe6a303e2c
Karem Darwesh <karemdarwesh1035@gmail.com>
2023-04-08 04:18:56 -0700

    print the name of the file followed by a new line

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - start point.
+ * @argc: number of arguments.
+ * @argv: array of arguments.
+ *
+ * Return: 0 success.
+ */
+
 int main (int argc __attribute__((__unused__)), char *argv[])
 {
         printf("%s\n", argv[0]);
