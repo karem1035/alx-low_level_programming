84a6b1444eddd019a66e256d317d540a18e3d9e4
Karem Darwesh <karemdarwesh1035@gmail.com>
2023-04-14 05:21:53 -0700

    free memory of 2d grid

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * free_grid - frees a memory location of a 2d grid.
+ * @grid: the array.
+ * @height: the hight of the grid.
+ *
+ * Return: nothing.
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	if (grid == NULL)
+	{
+		free(grid);
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+
+	free(grid[i]);
+}
