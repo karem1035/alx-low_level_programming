70c3f27956c4136e70efabe8d9a6720e338849a6
Karem Darwesh <karemdarwesh1035@gmail.com>
2023-04-17 12:40:29 -0700

    new type defined

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+# This is a directory contains my solution to alx tasks, 0x0E. C - Structures, typedef.
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,11 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
