3ce77692a2601086cb252e60a7b00d16327cf4fd
Karem Darwesh <karemdarwesh1035@gmail.com>
2023-04-17 12:44:26 -0700

    new type defined

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 
+/**
+ * struct dog - data type for dogs info.
+ * @name: its name.
+ * @age: its age.
+ * @owner: ins owner.
+ *
+ * Description: this is a new data type for dogs and its info.
+ */
+
 struct dog
 {
 	char *name;
