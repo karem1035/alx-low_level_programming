ea61a6250b3c80d11f6d65c2ae7443f203ee3fe0
Karem Darwesh <karemdarwesh1035@gmail.com>
2023-04-17 16:18:42 -0700

    creates a new variable with a struct data type

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,27 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * new_dog - creates a new dog.
+ * @name: name of the new dog.
+ * @age: age of the new dog.
+ * @owner: name of the owner of the new dog.
+ *
+ * Return: address of the new created dog.
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *my_dog = malloc(sizeof(dog_t));
+
+	if (my_dog == NULL)
+	{
+		return (NULL);
+	}
+
+	my_dog->name = name;
+	my_dog->age = age;
+	my_dog->owner = owner;
+
+	return (my_dog);
+}
