dc234874c601347f77c79128cb42147095fee6b7
Karem Darwesh <karemdarwesh1035@gmail.com>
2023-04-27 16:45:38 -0700

    Write a function that prints all the elements of a list_t list.

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,35 +1,28 @@
 #include "lists.h"
 
 /**
- * print_list - prints all liked list nodes.
- * @h: the first node pointer.
+ * print_list - function that prints all the elements of a list_t list.
  *
- * Return: number of nodes.
+ * @h: This take the value of the single list
+ *
+ * Return: Return: the number of nodes
+ *         If str is NULL, print [0] (nil)
  */
 
 size_t print_list(const list_t *h)
 {
-	unsigned int c = 0;
+	unsigned int count = 0;
 
 	if (h == NULL)
-	{
-		return (c);
-	}
-
-	while (h)
+		return (0);
+	while (h != NULL)
 	{
 		if (h->str == NULL)
-		{
-			printf("[0] (nil)\n");
-		}
+			printf("[%u] (nil)\n", h->len);
 		else
-		{
-			printf("[%d] %s\n", h->len, h->str);
-		}
-
+			printf("[%u] %s\n", h->len, h->str);
+		count += 1;
 		h = h->next;
-		c++;
 	}
-
-	return (c);
+	return (count);
 }
