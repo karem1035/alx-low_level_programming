70fbe922fc55bec53394e6a546a71003e7c15f51
Karem Darwesh <karemdarwesh1035@gmail.com>
2023-04-28 17:00:08 -0700

    a fucntion that executes before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * print_first - a funtion that excutes first before main.
+ * Return: void.
+ */
+
+void print_first(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+	       "I bore my house upon my back!\n");
+}
