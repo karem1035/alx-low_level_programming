57fd702b06185f3766cb5780ea122b81314fa356
Karem Darwesh <karemdarwesh1035@gmail.com>
2023-05-01 16:49:11 -0700

    a function that returns the number of elements in a linked listint_t list.

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -2,7 +2,7 @@
 /**
  * listint_len - returns the len of a list.
  * @h: a pointer to the first member of a list.
- * Retrun: number of nodes.
+ * Return: number of nodes.
  */
 size_t listint_len(const listint_t *h)
 {
