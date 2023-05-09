e59e2277f7b0d080ff4d52f77b4eb145c7a4f213
Karem Darwesh <karemdarwesh1035@gmail.com>
2023-05-09 06:14:51 -0700

    Create a function that creates a file.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,7 +10,7 @@ int create_file(const char *filename, char *text_content)
 	int fd, w, len;
 
 	len = strlen(text_content);
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_EXCL | O_TRUNC, 0600);
 	if (fd < 0 || filename == NULL)
 		return (-1);
 	if (text_content == NULL)
