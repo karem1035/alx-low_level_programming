b1c3e05cf4a591b9cc853c6524af5766c1772bf1
Karem Darwesh <karemdarwesh1035@gmail.com>
2023-05-10 00:35:27 -0700

    a program that copies the content of a file to another file.

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+/**
+ * main - a program that copies the content of a file to another file.
+ * @argc: number of arguments.
+ * @argv: array of arguments.
+ * Return: 0 when it succeed.
+ */
+int main(int argc, char *argv[])
+{
+	int fd1, fd2, r, w;
+	char *buffer[1024], *file1 = argv[1], *file2 = argv[2];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	fd1 = open(file1, O_RDONLY);
+	if (fd1 < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file1);
+		exit(98);
+	}
+	fd2 = open(file2, O_CREAT | O_WRONLY | O_TRUNC, 0677);
+	if (fd2 < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file2);
+		exit(99);
+	}
+	while ((r = read(fd1, buffer, 1024)) > 0)
+	{
+		w = write(fd2, buffer, r);
+		if (w != r)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file2);
+			exit(99);
+		}
+	}
+
+	if (close(fd1) < 0)
+	{
+		dprintf(STDERR_FILENO, "Can't close fd %d\n", fd1);
+	}
+	if (close(fd2) < 0)
+	{
+		dprintf(STDERR_FILENO, "Can't close fd %d\n", fd2);
+	}
+
+	return (0);
+}
