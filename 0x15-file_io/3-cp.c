dcbd36079b486129bea03ce441d183a7863ff2c0
Karem Darwesh <karemdarwesh1035@gmail.com>
2023-05-10 03:06:50 -0700

    a program that copies the content of a file to another file.

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
-int safe_close(int description);
 /**
  * main - a program that copies the content of a file to another file.
  * @argc: number of arguments.
@@ -14,59 +13,33 @@ int main(int argc, char *argv[])
 	int fd1, fd2, r, w;
 	char *buffer[1024], *file1 = argv[1], *file2 = argv[2];
 
-
 	if (argc != 3)
-	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
-	}
 	fd1 = open(file1, O_RDONLY);
 	if (fd1 < 0)
-	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file1);
 		exit(98);
-	}
-	fd2 = open(file2, O_CREAT | O_WRONLY | O_TRUNC | O_EXCL, 0664);
+	fd2 = open(file2, O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd2 < 0)
-	{
-		safe_close(fd1);
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file2);
 		exit(99);
-	}
 	while ((r = read(fd1, buffer, 1024)) > 0)
 	{
 		w = write(fd2, buffer, r);
 		if (w != r)
-		{
-			safe_close(fd1);
-			safe_close(fd2);
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file2);
 			exit(99);
-		}
 	}
-	if (safe_close(fd1) < 0)
-	{
+	if (r < 0)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file1);
+		exit(98);
+	if (close(fd1) < 0)
 		dprintf(STDERR_FILENO, "Can't close fd %d\n", fd2);
 		exit(100);
-	}
-	if (safe_close(fd2) < 0)
-	{
+	if (close(fd2) < 0)
 		dprintf(STDERR_FILENO, "Can't close fd %d\n", fd2);
 		exit(100);
-	}
-
 	return (0);
 }
 
-
-int safe_close(int description)
-{
-	int er;
-
-	er = close(description);
-	if (er < 0)
-	{
-		dprintf(STDERR_FILENO, "Can't close fd %d\n", description);
-	}
-	return (er);
-}
