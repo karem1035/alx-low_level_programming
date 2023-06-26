e4d750cb1bcdd435ee8d8db2d7755cd71d7bf459
Karem Darwesh <karemdarwesh1035@gmail.com>
2023-06-25 17:20:57 -0700

    new node

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -30,9 +30,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (add_dnodeint_end(h, n));
 
 	new_node->n = n;
-	new_node->next = current->next;
-	current->next = new_node;
 	new_node->prev = current;
-
+	new_node->next = current->next;
+	current->next->prev = new_node;
+	current->next = new_node;
 	return (new_node);
 }
