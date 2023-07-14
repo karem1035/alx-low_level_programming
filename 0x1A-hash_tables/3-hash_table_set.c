2f4859579d084b6859154ca23d8dff37b3ac3ad2
Karem Darwesh <karemdarwesh1035@gmail.com>
2023-07-13 17:44:10 -0700

    task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,7 +11,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index, i;
 	hash_node_t *new_node;
 
-	if (ht == NULL || key == NULL || *key == ;'\0')
+	if (ht == NULL || key == NULL || *key == '\0')
 		return (0);
 
 	index = key_index((const unsigned char *) key, ht->size);
