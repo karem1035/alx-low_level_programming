e345aa74413754ed8c4b66481049592a0f99fdd2
Karem Darwesh <karemdarwesh1035@gmail.com>
2023-07-13 17:36:32 -0700

    task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,34 +1,41 @@
 #include "hash_tables.h"
-
+/**
+ * hash_table_set - set a node at hash tables.
+ * @ht: the table pointer.
+ * @key: the key of the node.
+ * @value: the value of the key.
+ * Return: 0 if failed 1 if succeded
+ */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-        unsigned long int index, i;
-        hash_node_t *new_node;
+	unsigned long int index, i;
+	hash_node_t *new_node;
 
 	if (ht == NULL)
 		return (0);
 
-        index = key_index((const unsigned char *) key, ht->size);
+	index = key_index((const unsigned char *) key, ht->size);
 
-        new_node = malloc(sizeof(hash_node_t));
-        if (new_node == NULL)
-                return (1);
-        new_node->key = strdup(key);
-        new_node->value = strdup(value);
-        if (ht->array[index] == NULL)
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return (1);
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+	if (ht->array[index] == NULL)
 	{
-                ht->array[index] = new_node;
+		ht->array[index] = new_node;
 		return (1);
 	}
-        else
-                while (i < ht->size)
-                {
-                        if (ht->array[i] == NULL)
+	else
+	{
+		while (i < ht->size)
+		{
+			if (ht->array[i] == NULL)
 			{
-                                ht->array[i] = new_node;
+				ht->array[i] = new_node;
 				return (1);
 			}
-                        i++;
-                }
-        return (1);
+		}
+	}
+	return (1);
 }
