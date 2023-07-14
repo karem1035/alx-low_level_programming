013f502718cfe9ea702d88de013fe99a50534d65
Karem Darwesh <karemdarwesh1035@gmail.com>
2023-07-13 17:43:31 -0700

    task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,16 +11,26 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index, i;
 	hash_node_t *new_node;
 
-	if (ht == NULL)
+	if (ht == NULL || key == NULL || *key == ;'\0')
 		return (0);
 
 	index = key_index((const unsigned char *) key, ht->size);
 
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
-		return (1);
+		return (0);
+
 	new_node->key = strdup(key);
 	new_node->value = strdup(value);
+
+	if (new_node->key == NULL || new_node->value == NULL)
+	{
+		free(new_node->key);
+		free(new_node->value);
+		free(new_node);
+		return 0;
+	}
+
 	if (ht->array[index] == NULL)
 	{
 		ht->array[index] = new_node;
