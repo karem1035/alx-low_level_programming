93ac62e5186e1883400abf9fff4a7153f687cace
Karem Darwesh <karemdarwesh1035@gmail.com>
2023-07-14 22:49:28 -0700

    hash set last

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -9,42 +9,47 @@ int create_node(hash_node_t *new_node, const char *key, const char *value);
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int index;
-	hash_node_t *new_node, *current;
+        unsigned long int index;
+        hash_node_t *new_node, *current;
 
-	if (ht == NULL || key == NULL || *key == '\0')
-		return (0);
+        if (ht == NULL || key == NULL || *key == '\0')
+                return (0);
 
-	index = key_index((const unsigned char *) key, ht->size);
-	new_node = malloc(sizeof(hash_node_t));
+        index = key_index((const unsigned char *)key, ht->size);
+        new_node = malloc(sizeof(hash_node_t));
 
-	if (create_node(new_node, key, value))
-		return (0);
+        if (create_node(new_node, key, value))
+                return (0);
 
-	if (ht->array[index] == NULL)
-	{
-		ht->array[index] = new_node;
-		return (1);
-	}
-	else
-	{
-		current = ht->array[index];
-		while (current != NULL)
-		{
-			if (strcmp(current->key, key) == 0)
-			{
-				free(current->value);
-				current->value = strdup(value);
-				free(new_node->key);
-				free(new_node->value);
-				free(new_node);
-				return (1);
-			}
-			current = current->next;
-		}
-		ht->array[index] = new_node;
-	}
-	return (1);
+        if (ht->array[index] == NULL)
+        {
+                ht->array[index] = new_node;
+                return (1);
+        }
+        else
+        {
+                current = ht->array[index];
+                while (current != NULL)
+                {
+                        if (strcmp(current->key, key) == 0)
+                        {
+                                free(current->value);
+                                current->value = strdup(value);
+                                free(new_node->key);
+                                free(new_node->value);
+                                free(new_node);
+                                return (1);
+                        }
+                        if (current->next == NULL)
+                        {
+                                current->next = new_node;
+                                break;
+                        }
+                                current = current->next;
+                }
+                ht->array[index] = new_node;
+        }
+        return (1);
 }
 /**
  * create_node - creates and check the new node.
@@ -55,19 +60,19 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
  */
 int create_node(hash_node_t *new_node, const char *key, const char *value)
 {
-	if (new_node == NULL)
-		return (1);
+        if (new_node == NULL)
+                return (1);
 
-	new_node->key = strdup(key);
-	new_node->value = strdup(value);
+        new_node->key = strdup(key);
+        new_node->value = strdup(value);
 
-	if (new_node->key == NULL || new_node->value == NULL)
-	{
-		free(new_node->key);
-		free(new_node->value);
-		free(new_node);
-		return (1);
-	}
+        if (new_node->key == NULL || new_node->value == NULL)
+        {
+                free(new_node->key);
+                free(new_node->value);
+                free(new_node);
+                return (1);
+        }
 
-return (0);
+        return (0);
 }
