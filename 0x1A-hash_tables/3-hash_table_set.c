02e53eeb4b3780376f6b054a5b0c7aefab080063
Karem Darwesh <karemdarwesh1035@gmail.com>
2023-07-14 23:13:01 -0700

    2023

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -42,7 +42,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			}
 			current = current->next;
 		}
+		current = ht->array[index];
 		ht->array[index] = new_node;
+		new_node->next = current;
 	}
 	return (1);
 }
