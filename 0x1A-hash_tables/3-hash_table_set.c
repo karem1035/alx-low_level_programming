b791d092a51fd01b04b899ee20a457bf35b05344
Karem Darwesh <karemdarwesh1035@gmail.com>
2023-09-07 19:51:06 +0300

    adding node the ht

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,9 +1,33 @@
 #include "hash_tables.h"
 /**
- * 
-*/
+ * hash_table_set - adding a node at the hash table
+ * @key: the key of the node.
+ * @value: the value of the key.
+ * Return: 0 if it is succeded 1 if it is not   
+ */
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	
+	unsigned long int index = key_index(key, ht->size);
+	hash_node_t *node;
+
+	node = malloc(sizeof(hash_node_t));
+	if (!node)
+		return (0);
+
+	node->key = key;
+	node->value = value;
+
+	if (ht->array[index] != NULL)
+	{
+		node->next = ht->array[index];
+		ht->array[index] = node;
+	}
+	else
+	{
+		ht->array[index] = node;
+		node->next = NULL;
+	}
+
+	return (0);
 }
