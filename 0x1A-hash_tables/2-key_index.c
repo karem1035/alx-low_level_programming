3e84f5ac6a5f7bffcab608b7a191cc7c93db87a8
Karem Darwesh <karemdarwesh1035@gmail.com>
2023-09-07 17:49:21 +0300

    task 3

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,5 +1,10 @@
 #include "hash_tables.h"
-
+/**
+ * key_index - gets a key and returns its index on the hash table
+ * @key: the key input
+ * @size: the size of the hash table
+ * Return: the index on the table
+*/
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	unsigned long int index;
