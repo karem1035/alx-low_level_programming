f4a5ca8a556ea1e745c86a06f61d35142b8fad6b
Karem Darwesh <karemdarwesh1035@gmail.com>
2023-09-07 19:57:41 +0300

    task edited

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -3,20 +3,32 @@
  * hash_table_set - adding a node at the hash table
  * @key: the key of the node.
  * @value: the value of the key.
- * Return: 0 if it is succeded 1 if it is not   
+ * Return: 0 if it is succeded 1 if it is not
  */
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int index = key_index(key, ht->size);
+	unsigned long int index = key_index((const unsigned char *)key, ht->size);
 	hash_node_t *node;
+	char *ckey, *cvalue;
 
 	node = malloc(sizeof(hash_node_t));
 	if (!node)
 		return (0);
 
-	node->key = key;
-	node->value = value;
+	ckey = malloc(sizeof(key));
+	if (!ckey)
+		return (0);
+
+	cvalue = malloc(sizeof(value));
+	if (!cvalue)
+	{
+		free(node);
+		free(ckey);
+		return (0);
+	}
+	node->key = strcpy(ckey, key);
+	node->value = strcpy(cvalue, value);
 
 	if (ht->array[index] != NULL)
 	{
