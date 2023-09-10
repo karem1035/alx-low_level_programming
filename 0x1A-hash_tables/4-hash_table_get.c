341ffcf15bae164b138fd85c7a1d13abd0a74e25
Karem Darwesh <karemdarwesh1035@gmail.com>
2023-09-11 00:29:21 +0300

    task 4 finally

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -8,12 +8,21 @@
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int index;
+	hash_node_t *tmp;
 
 	if (ht == NULL || key == NULL || *key == '\0')
 		return (NULL);
 
 	index = key_index((const unsigned char *)key, ht->size);
 	if (ht->array[index])
-		return (ht->array[index]->value);
+	{
+		tmp = ht->array[index];
+		while (tmp)
+		{
+			if (strcmp(tmp->key, key) == 0)
+				return (tmp->value);
+			tmp = tmp->next;
+		}
+	}
 	return (NULL);
 }
