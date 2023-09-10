d97fef5d35c27acf4af113205d7273c1515be7a7
Karem Darwesh <karemdarwesh1035@gmail.com>
2023-09-11 01:01:24 +0300

    task 6

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,25 @@
+#include "hash_tables.h"
+/**
+ * hash_table_delete - deletes a hash table.
+ * @ht: the hash table pointer.
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i = 0;
+	hash_node_t *current_node, *next_node;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		current_node = ht->array[i];
+		while (current_node)
+		{
+			next_node = current_node->next;
+			free(current_node->key);
+			free(current_node->value);
+			free(current_node);
+			current_node = next_node;
+		}
+	}
+	free(ht->array);
+	free(ht)
+}
