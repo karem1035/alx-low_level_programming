8d5ec7a91fec5be7fb56a8ac9676ce73cda59df6
karem darwesh <karemdarwesh1035@gmail.com>
2024-04-09 08:21:16 +0200

    Task 1 binary search

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,46 +1,39 @@
 #include "search_algos.h"
 
-void print_arry(int *array, size_t left, size_t right);
-
+/**
+ * binary_search - search an array with a binary search.
+ * @array: the array to be searched.
+ * @size: the size of the array.
+ * @value: the value to be searched.
+ * Return: index of the value in the array or -1 if error.
+ */
 int binary_search(int *array, size_t size, int value)
 {
-	size_t left = 0, right = size, mid;
+	int left = 0, right = size - 1, i, mid;
 
 	if (!array)
 		return (-1);
 
 	while (left <= right)
 	{
-		print_arry(array, left, right);
+		printf("Searching in array: ");
+		for (i = left; i <= right; i++)
+		{
+			if (i < right)
+				printf("%d, ", array[i]);
+			else
+				printf("%d", array[i]);
+		}
+		printf("\n");
 
 		mid = left + (right - left) / 2;
+
 		if (array[mid] == value)
-			return mid;
+			return (mid);
 		if (array[mid] < value)
 			left = mid + 1;
-
-		if (array[mid] > value)
+		else
 			right = mid - 1;
 	}
 	return (-1);
-}
-
-/**
- * print_array - print a division of array
- * @left: the starting point to be printed
- * @right: the end point to print to
- */
-void print_arry(int *array, size_t left, size_t right)
-{
-	size_t i;
-
-	printf("Searching in array: ");
-	for (i = left; i < right; i++)
-	{
-
-		printf("%d", array[i]);
-		if (i < right - 1)
-			printf(", ");
-	}
-	printf("\n");
 }
