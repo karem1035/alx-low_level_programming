9c34bce849d30e90c14485c38eebd2335573ee46
karem darwesh <karemdarwesh1035@gmail.com>
2024-04-10 10:46:40 +0200

    Task 104

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -1,4 +1,5 @@
 #include "search_algos.h"
+int advanced_binary_recursive(int *array, int l, int r, int value);
 
 /**
  * advanced_binary - returns the first occurence of value in array
@@ -10,30 +11,36 @@
  */
 int advanced_binary(int *array, size_t size, int value)
 {
-	int mid, l = 0, r = size - 1, i;
-
 	if (!array)
 		return (-1);
 
-	while (l <= r)
-	{
-		mid = l + (r - l) / 2;
+	return (advanced_binary_recursive(array, 0, size - 1, value));
+}
 
-		printf("Searching in array: ");
-		for (i = l; i <= r; i++)
-		{
-			if (i + 1 > r)
-				printf("%d\n", array[i]);
-			else
-				printf("%d, ", array[i]);
-		}
-		if (array[mid] == value && (mid == 0 || array[mid - 1] < value))
-			return (mid);
+/**
+ * advanced_binary_recursive - using recusive on binary search.
+ * @array: the arrray to lookup.
+ * @l: the left index of the search area on the array.
+ * @r: the right index of the search area on the array.
+ * @value: the value we are looking at.
+ * Return: the index of the value on the array or -1.
+ */
+int advanced_binary_recursive(int *array, int l, int r, int value)
+{
+	int mid, i;
 
-		else if (array[mid] < value)
-			l = mid + 1;
-		else
-			r = mid - 1;
-	}
-	return (-1);
+	if (r < l)
+		return (-1);
+	mid = l + (r - l) / 2;
+	printf("Searching in array: ");
+	for (i = l; i < r; i++)
+		printf("%d, ", array[i]);
+	printf("%d\n", array[i]);
+
+	if (array[mid] == value)
+		return (mid);
+	else if (array[mid] < value)
+		return (advanced_binary_recursive(array, mid + 1, r, value));
+	else
+		return (advanced_binary_recursive(array, l, mid - 1, value));
 }
