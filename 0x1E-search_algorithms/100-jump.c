b05d5273e8881f76bfd348fb6e89356916ae1bab
karem darwesh <karemdarwesh1035@gmail.com>
2024-04-11 18:49:34 +0200

    task size = 0 added 2

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -36,7 +36,9 @@ int jump_search(int *array, size_t size, int value)
 
 	for (; i <= end; i++)
 	{
-		if (i + 1 > (int)size)
+		printf("Value checked array[%d] = [%d]\n", i, i);
+
+		if (i + 1 > (int)size - 1)
 			break;
 		if (array[i] == value)
 			return (i);
